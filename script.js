c8de5191e048dfdaf593762eb76e5fb0f44805fd
kambdan <juancambi1319@gmail.com>
2024-07-07 10:36:52 -0500

    J.C UPDATE: promo

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-// Mostrar el diálogo al cargar la página
 window.onload = function() {
     document.getElementById('dialog').style.display = 'block';
 
-    // Añadir el manejador de eventos al botón 'Empezar viaje'
     document.getElementById('start-trip').addEventListener('click', function() {
         window.location.href = 'https://play.google.com/store/apps/details?id=com.latam.xp&hl=es_EC';
     });
 };
 
-// Función para cerrar el diálogo
 function closeDialog() {
     document.getElementById('dialog').style.display = 'none';
+}
+
+let slideIndex = 0;
+const items = document.querySelectorAll('.carousel-item');
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    if (n >= items.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = items.length - 1;
+    }
+    for (let i = 0; i < items.length; i++) {
+        items[i].style.display = 'none';
+    }
+    items[slideIndex].style.display = 'block';
 }
